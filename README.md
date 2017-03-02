13443eac89a7b413369775ff3d0475f134514c56
AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
2017-03-02 13:54:24 +0900

    make git project

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::MadokaMagica - It's new $module
+
+# SYNOPSIS
+
+    use Acme::MadokaMagica;
+
+# DESCRIPTION
+
+Acme::MadokaMagica is ...
+
+# LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
---
+++ lib/Acme/MadokaMagica.pm
@@ -0,0 +1,39 @@
+package Acme::MadokaMagica;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::MadokaMagica - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::MadokaMagica;
+
+=head1 DESCRIPTION
+
+Acme::MadokaMagica is ...
+
+=head1 LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+AnaTofuZ E<lt>e155730@ie.u-ryukyu.ac.jpE<gt>
+
+=cut
+
