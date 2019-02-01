a96e8cdda2436f087a4d07224a21104a7212b48b
Takahiro SHIMIZU <anatofuz@gmail.com>
2019-02-01 17:35:21 +0900

    Mouse 落として use parent

--- lib/Acme/MadokaMagica/TvMembers/AkemiHomura.pm
+++ lib/Acme/MadokaMagica/TvMembers/AkemiHomura.pm
@@ -1,9 +1,7 @@
 package Acme::MadokaMagica::TvMembers::AkemiHomura;
-use Mouse;
-extends 'Acme::MadokaMagica::TvMembers';
+use parent qw/Acme::MadokaMagica::TvMembers/;
 use utf8;
 
-no Mouse;
 
 1;
 
